4c9d3353523d8a6f0635b15191ae3fa8a0f0bb07
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-16 20:12:58 -0400

    StockAPIVersion 1 Stockapiv1ApplicationTests.java

---
+++ src/test/java/com/jay/stockapiv1/Stockapiv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.stockapiv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stockapiv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
