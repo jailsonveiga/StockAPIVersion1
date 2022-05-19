bf592245ca930a53c0a43177a9a066aa18480b40
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-19 17:51:24 -0400

    StockAPIVersion 1 OverviewController.java

--- src/main/java/com/jay/stockapiv1/controllers/OverviewController.java
+++ src/main/java/com/jay/stockapiv1/controllers/OverviewController.java
@@ -1,12 +1,14 @@
 package com.jay.stockapiv1.controllers;
 
+import com.jay.stockapiv1.models.Overview;
+import com.jay.stockapiv1.repositories.OverviewRepository;
+import com.jay.stockapiv1.utils.ApiErrorHandling;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -16,35 +18,143 @@ public class OverviewController {
     @Autowired
     private Environment env;
 
+    @Autowired
+    private OverviewRepository overviewRepository;
+
     private final String BASE_URL = "https://www.alphavantage.co/query?function=OVERVIEW";
 
+    // http://localhost:8080/api/overview/test
     @GetMapping("/test")
-    public ResponseEntity<?> testOverview(RestTemplate restTemplate) {
+    private ResponseEntity<?> testOverview(RestTemplate restTemplate) {
         try {
 
             String url = BASE_URL + "&symbol=IBM&apikey=demo";
 
-            String responseBody = restTemplate.getForObject(url, String.class);
+            Overview responseBody = restTemplate.getForObject(url, Overview.class);
 
             return ResponseEntity.ok(responseBody);
 
+        } catch(IllegalArgumentException e) {
+
+            return ApiErrorHandling.customApiError("Error In test overview: Check URL used for AV Request", HttpStatus.INTERNAL_SERVER_ERROR);
+
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body(e.getMessage());
+
+            return ApiErrorHandling.genericApiError(e);
+
         }
     }
 
+    //TEST UPLOAD TO DB
+    @PostMapping("/test")
+    private ResponseEntity<?> testUploadOverview(RestTemplate restTemplate) {
+        try {
+
+            String url = BASE_URL + "&symbol=IBM&apikey=demo";
+
+            Overview responseBody = restTemplate.getForObject(url, Overview.class);
+
+            if (responseBody == null) {
+
+                return ApiErrorHandling.customApiError("Did not receive response from AV", HttpStatus.INTERNAL_SERVER_ERROR);
+
+            }
+
+            else if (responseBody.getSymbol() == null) {
+
+                return ApiErrorHandling.customApiError("No Data Retrieved From AV", HttpStatus.NOT_FOUND);
+
+            }
+
+            Overview savedOverview = overviewRepository.save(responseBody);
+
+            return ResponseEntity.ok(savedOverview);
+
+        } catch (DataIntegrityViolationException e) {
+
+            return ApiErrorHandling.customApiError("Can not upload duplicate Stock data", HttpStatus.BAD_REQUEST);
+
+        }
+        catch(IllegalArgumentException e) {
+
+            return ApiErrorHandling.customApiError("Error In test overview: Check URL used for AV Request", HttpStatus.INTERNAL_SERVER_ERROR);
+
+        } catch (Exception e) {
+
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+    }
+
+    // http://localhost:8080/api/overview/{symbol}
     @GetMapping("/{symbol}")
     public ResponseEntity<?> dynamicOverview(RestTemplate restTemplate, @PathVariable String symbol) {
         try {
 
-            String url = BASE_URL + "&symbol="+symbol+"&apikey="+env.getProperty("AV_API_KEY");
+            String apiKey = env.getProperty("AV_API_KEY");
 
-            String responseBody = restTemplate.getForObject(url, String.class);
+            String url = BASE_URL + "&symbol=" + symbol + "&apikey=" + apiKey;
+
+            System.out.println(url);
+
+            Overview responseBody = restTemplate.getForObject(url, Overview.class);
+
+            if (responseBody == null) {
+
+                return ApiErrorHandling.customApiError("Did not receive response from AV", HttpStatus.INTERNAL_SERVER_ERROR);
+
+            }
+
+            else if (responseBody.getSymbol() == null) {
+
+                return ApiErrorHandling.customApiError("Invalid Stock Symbol: " + symbol, HttpStatus.NOT_FOUND);
+
+            }
 
             return ResponseEntity.ok(responseBody);
 
         } catch (Exception e) {
-            return ResponseEntity.internalServerError().body(e.getMessage());
+
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+    }
+
+    @PostMapping("/{symbol}")
+    public ResponseEntity<?> uploadOverview(RestTemplate restTemplate, @PathVariable String symbol) {
+        try {
+
+            String apiKey = env.getProperty("AV_API_KEY");
+
+            String url = BASE_URL + "&symbol=" + symbol + "&apikey=" + apiKey;
+
+            Overview responseBody = restTemplate.getForObject(url, Overview.class);
+
+            if (responseBody == null) {
+
+                return ApiErrorHandling.customApiError("Did not receive response from AV", HttpStatus.INTERNAL_SERVER_ERROR);
+
+            }
+
+            else if (responseBody.getSymbol() == null) {
+
+                return ApiErrorHandling.customApiError("Invalid Stock Symbol: " + symbol, HttpStatus.NOT_FOUND);
+
+            }
+
+            Overview savedOverview = overviewRepository.save(responseBody);
+
+            return ResponseEntity.ok(savedOverview);
+
+        } catch (DataIntegrityViolationException e) {
+
+            return ApiErrorHandling.customApiError("Can not upload duplicate Stock data", HttpStatus.BAD_REQUEST);
+
+        }
+        catch (Exception e) {
+
+            return ApiErrorHandling.genericApiError(e);
+
         }
     }
 }
