edad02254fa3dfc17c4781c1faa099527e6f7a52
jailsonveiga <jmdaveiga2012@gmail.com>
2022-05-19 17:50:51 -0400

    StockAPIVersion 1 ApiErrorHandling.java

--- src/main/java/com/jay/stockapiv1/utils/ApiErrorHandling.java
+++ src/main/java/com/jay/stockapiv1/utils/ApiErrorHandling.java
@@ -1,4 +1,21 @@
 package com.jay.stockapiv1.utils;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class ApiErrorHandling {
+
+    public static ResponseEntity<?> genericApiError (Exception e) {
+
+        System.out.println(e.getMessage());
+        System.out.println(e.getClass());
+        return ResponseEntity.internalServerError().body(e.getMessage());
+
+    }
+
+    public static ResponseEntity<?> customApiError (String message, HttpStatus status) {
+
+        return ResponseEntity.status(status).body(message);
+
+    }
 }
